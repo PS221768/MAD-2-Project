1eca98ad3a1f860bc5dfab311975afd5ec94accc
ps217531 <ps217531@edu.summacollege.nl>
2022-05-19 15:22:21 +0200

    navigation on every page

--- Componenten/Singleplayer.js
+++ Componenten/Singleplayer.js
@@ -1,15 +1,60 @@
+import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native';
 
-import { View, Text } from 'react-native'
-import React from 'react'
-import  Menu from "./Menu";
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const Singleplayer = ({navigation}) => {
+const Stack = createStackNavigator();
+// import { Image } from 'react-native-web';
+const staticImage = require("/MAD2/TTTM/assets/logo.png");
+export default function App({navigation}) {
   return (
-    
-     
-      <Menu></Menu>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.menu}>
 
-  )
+        <Image style={styles.imgLogo} source={staticImage}></Image>
+        <TouchableOpacity  mode="contained" onPress={() => navigation.navigate("Multiplayer")}>
+          <Text style={styles.text}>Multiplayer</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
 }
 
-export default Singleplayer
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  
+  },
+  text: {
+    color: 'red',
+    padding:5,
+    fontSize: 20,
+    marginLeft:20,
+    marginRight:20,
+  },
+  menu: {
+    padding: 20,
+    backgroundColor: 'blue',
+    flexDirection: "row",
+    alignSelf: 'flex-center',
+
+   
+    height: '10%',
+    justifyContent: 'top',
+    marginTop: 20,
+
+  },
+
+  imgLogo: {
+    width: 40,
+    height: 40,
+
+  },
+});
